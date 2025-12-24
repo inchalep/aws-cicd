93edad93475cd376b059ac351fe92021de6b7fa7
idnyaneshwaraxioned <dnyaneshwari@axioned.com>
2025-12-25 00:23:48 +0530

    added docker and kubernetes

--- src/server.js
+++ src/server.js
@@ -15,3 +15,8 @@ app.get('/api',(req,res)=>{
 app.listen(8000,()=>{
   console.log('App is running on port', 8000)
 })
+//$ kubectl create deployment aws-cicd --port=80 --image=dnyaneshwarinchale/aws-cicd:1.0 --dry-run=client -o yaml > deployment.yaml
+// $ kubectl apply -f deployment.yaml
+//$ kubectl expose deployment aws-cicd --port=80 --target-port=8000 --type=LoadBalancer --dry-run=client -o yaml > service.yaml
+// $ kubectl apply -f service.yaml
+// inikube service aws-cicd-service --url
